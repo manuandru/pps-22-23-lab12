980b510cca502b2a12b0fbbeddb4038043ca5cba
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-05-26 08:45:16 +0200

    feat(2): foldleft

--- src/main/prolog/es.pl
+++ src/main/prolog/es.pl
@@ -14,3 +14,24 @@ map([H|T], M, [H2|T2]) :- map(T, M, T2), copy_term(M, mapper(H, H2, OP)), call(O
 filter([], _, []).
 filter([H|T], F, [H|T2]) :- filter(T, F, T2), copy_term(F, predicate(H, Test)), call(Test), !.
 filter([H|T], F, T2) :- filter(T, F, T2).
+
+
+
+% reduce(+L, +OP, -O)
+% where OP=op(I1, I2, BINARY_OP, O)
+% e.g. OP=op(X, Y, O is X + Y, O)
+% reduce([10,20,30], 0, op(X, Y, O is X + Y, O), R). -> R / 60.
+reduce([H|T], OP, O) :- reduce(T, H, OP, O).
+reduce([], Acc, _, Acc).
+reduce([H|T], Acc, Op, R) :- copy_term(Op, op(H, Acc, CopyOP, O)), call(CopyOP), reduce(T, O, Op, R).
+
+
+
+% foldleft(+L, +Acc, +OP, -O)
+% where Acc is the default value
+% where OP=op(I1, I2, BINARY_OP, O)
+% e.g. OP=op(X, Y, O is X + Y, O)
+% foldleft([10, 20, 30], 0, op(X, Y, O is X + Y, O), R). -> R / 60
+% foldleft([10, 20, 30], [],op(H, T, O = [H|T], O), R). -> R / [30, 20, 10]
+foldleft([], Acc, _, Acc).
+foldleft([H|T], Acc, Op, R) :- copy_term(Op, op(H, Acc, CopyOP, O)), call(CopyOP), foldleft(T, O, Op, R).
